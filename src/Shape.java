18f39543c1b8d1a02f8ca0254592a8d8b8e17da3
mahtabmann2003 <47609548+mahtabmann2003@users.noreply.github.com>
2020-11-08 15:05:07 -0500

    Test

--- src/Shape.java
+++ src/Shape.java
@@ -1,2 +1,10 @@
+
+import java.util.Random;
+
 public class Shape {
+
+    protected enum Tetrominoes {
+        NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredShape
+    }
+
 }
