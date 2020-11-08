0772a0cb9d88ca2c158e0c712302356481d908f1
mahtabmann2003 <47609548+mahtabmann2003@users.noreply.github.com>
2020-11-08 16:54:38 -0500

    Finished Shape Commands, and Creations

--- src/Shape.java
+++ src/Shape.java
@@ -1,10 +1,113 @@
-
-import java.util.Random;
+import java.util.*;
 
 public class Shape {
 
     protected enum Tetrominoes {
         NoShape, ZShape, SShape, LineShape, TShape, SquareShape, LShape, MirroredShape
     }
+
+    private Tetrominoes pieceShape;
+    private int[][] coords;
+
+    public Shape() {
+
+        coords = new int[4][2];
+        setShape(Tetrominoes.NoShape);
+    }
+    void setShape(Tetrominoes shape) {
+
+        int [][][] coordsTable = new int[][][] {
+                {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
+                {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
+                {{0, -1}, {0, 0}, {1, 0}, {1, 1}},
+                {{0, -1}, {0, 0}, {0, 1}, {0, 2}},
+                {{-1, 0}, {0, 0}, {1, 0}, {0, 1}},
+                {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
+                {{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
+                {{1, -1}, {0, -1}, {0, 0}, {0, 1}}
+        };
+
+        for (int i = 0; i < 4; i++) {
+
+            System.arraycopy(coordsTable[shape.ordinal()], 0 , coords, 0, 4);
+
+        }
+
+        pieceShape = shape;
+    }
+
+    private void setX (int index, int x) {
+
+        coords[index][0] = x;
+    }
+    private void setY(int index, int y) {
+
+        coords[index][1] = y;
+    }
+    int x(int index) {
+
+        return coords[index][0];
+    }
+    int y (int index) {
+
+        return coords[index][1];
+    }
+    Tetrominoes getShape() {
+
+        return pieceShape;
+    }
+    void setRandomShape() {
+
+        var r = new Random();
+        int x = Math.abs(r.nextInt()) % 7 + 1;
+
+        Tetrominoes[] values = Tetrominoes.values();
+        setShape(values[x]);
+    }
+    public int minX() {
+        int m = coords[0][0];
+
+        for (int i = 0; i < 4; i++) {
+            m = Math.max(m, coords[i][0]);
+        }
+        return m;
+    }
+    int minY () {
+        int m = coords[0][1];
+
+        for (int i = 0; i < 4; i++) {
+            m = Math.min(m, coords[i][1]);
+        }
+        return m;
+    }
+    Shape rotateRight() {
+        if (pieceShape == Tetrominoes.SquareShape) {
+            return this;
+        }
+        var result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; i++) {
+            result.setX(i, -y(i));
+            result.setY(i, x(i));
+        }
+        return result;
+    }
+    Shape rotateLeft() {
+        if (pieceShape == Tetrominoes.SquareShape) {
+            return this;
+        }
+        var result = new Shape();
+        result.pieceShape = pieceShape;
+
+        for (int i = 0; i < 4; i++) {
+            result.setX(i, y(i));
+            result.setY(i, -x(i));
+        }
+        return result;
+    }
+
+
+
 
 }
