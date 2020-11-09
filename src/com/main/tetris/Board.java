65edda6804d0479f2cc593a366dd286f002fd496
devpatel2003 <mahtabmann@Mahtabs-iMac.local>
2020-11-08 19:32:15 -0500

    Imported Classes

--- src/com/main/tetris/Board.java
+++ src/com/main/tetris/Board.java
@@ -1,3 +1,37 @@
+package com.main.tetris;
 
-public class Board {
+import com.main.tetris.Shape.Tetrominoes;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+
+public class Board extends JPanel {
+    private final int BOARD_HEIGHT = 22;
+    private final int BOARD_WIDTH = 10;
+    private final int PERIOD_INTERVAL = 300;
+
+    private Timer timer;
+    private boolean isFallingFinished = false;
+    private boolean isPaused = false;
+    private int numLinesRemoved = 0;
+    private int curX = 0;
+    private int curY = 0;
+    private JLabel statusbar;
+    private Shape curPiece;
+    private Tetrominoes[] board;
+
+    public Board(Tetris parent) {
+        initBoard(parent);
+    }
+    private void initBoard(Tetris parent) {
+        setFocusable(true);
+        statusbar = parent.getStatusBar();
+        addKeyListener(new TAdapter());
+    }
 }
